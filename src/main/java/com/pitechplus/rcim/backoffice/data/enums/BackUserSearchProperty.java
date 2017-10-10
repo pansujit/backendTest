8e4bd5cd1152fac2fa9e83921fa53cf689d0759b
sujitpandey <sujitpandey@sujits-mbp>
2017-10-10 11:38:54 +0200

    changes made

--- src/main/java/com/pitechplus/rcim/backoffice/data/enums/BackUserSearchProperty.java
+++ src/main/java/com/pitechplus/rcim/backoffice/data/enums/BackUserSearchProperty.java
@@ -4,7 +4,7 @@ package com.pitechplus.rcim.backoffice.data.enums;
  * Created by dgliga on 08.03.2017.
  */
 public enum BackUserSearchProperty {
-    FIRSTNAME("firstName"), LASTNAME("lastName"), EMAIL("login"), ROLE("role"), ENABLED("enabled");
+    FIRSTNAME("firstName"), LASTNAME("lastName"), EMAIL("login"), ROLE("role"), ENABLED("enabled"),VIP("vip");
 
     private String value;
 
